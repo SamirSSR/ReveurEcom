e6cc8e03379dc336b68eb7492d595231f84a573b
SamirSSR <rsamiran717@gmail.comm>
2025-06-14 08:37:06 +0530

    Dropdown mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 document.getElementById('menu-toggle').addEventListener('click', () => {
   document.getElementById('mobile-menu').classList.toggle('hidden');
+  document.getElementById('desktop-menu').classList.toggle('hidden');
 });
